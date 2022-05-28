3b1b85b12e3c05947f32c0631b1728a7b69eb7ed
vera4056 <103599166+vera4056@users.noreply.github.com>
2022-05-28 13:49:21 +0300

    initial commit

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        int initialBalance = 400;
+        int deposit = 1300;
+        int currentBalance = initialBalance + deposit;
+        int bonus = currentBalance / 100; // 100 %
+        int y = bonus;
+        if (currentBalance > 1001) {
+            y = bonus;
+        }
+        if (currentBalance < 1001) {
+            y = 0;
+        }
+        System.out.println("Итоговый баланс =" + currentBalance);
+        System.out.println("Бонус =" + y);
+    }
+}public class main {
+}
